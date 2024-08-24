1469084ba5e986307f231a4acde3ee75956f08d5
DeclanLacey <declan.lacey@hotmail.com>
2024-08-24 15:40:42 -0500

    footer and header styling

--- src/app/app/app.routes.ts
+++ src/app/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { AboutPageComponent } from '../about-page/about-page.component';
+import { LocationPageComponent } from '../location-page/location-page.component';
+import { ContactPageComponent } from '../contact-page/contact-page.component';
+import { HomePageComponent } from '../home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'about',
+        component: AboutPageComponent
+    },
+    {
+        path: 'locations',
+        component: LocationPageComponent
+    },
+    {
+        path: 'contact',
+        component: ContactPageComponent
+    },
+    {
+        path: '',
+        component: HomePageComponent
+    }
+
+];
