5c9e7adde0a6ea69283a27612f2b39e6cdf2d29c
DeclanLacey <declan.lacey@hotmail.com>
2024-08-24 17:31:38 -0500

    contact link card styling

--- src/app/contact-link-card/contact-link-card.component.ts
+++ src/app/contact-link-card/contact-link-card.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-contact-link-card',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, CommonModule, RouterLink],
   templateUrl: './contact-link-card.component.html',
   styleUrl: './contact-link-card.component.css'
 })
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,14 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Component, OnChanges, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink, RouterOutlet } from '@angular/router';
+import { ContactLinkCardComponent } from "../contact-link-card/contact-link-card.component";
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, RouterLink],
+  imports: [RouterOutlet, CommonModule, RouterLink, ContactLinkCardComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
-
+export class FooterComponent{
+ 
 }
