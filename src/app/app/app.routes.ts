f17556d54c49d6c3d60cf76a9c8d7244d550ef27
DeclanLacey <declan.lacey@hotmail.com>
2024-08-26 11:18:18 -0500

    styling and data for design page

--- src/app/app/app.routes.ts
+++ src/app/app/app.routes.ts
@@ -26,102 +26,17 @@ export const routes: Routes = [
     {
         path: 'web-design',
         component: DesignPageComponent,
-        data: [
-            {
-                "name": "Web Design",
-                "desc": "We build websites that serve as powerful marketing tools and bring memorable brand experiences."
-            },
-            {
-                "name": "express",
-                "desc": "A multi-carrier shipping website for e-commerce businesses",
-                "image": "/web-design/image-express.jpg"
-            },
-            {
-                "name": "transfer",
-                "desc": "Site for low-cost money transfers and sending money within seconds",
-                "image": "/web-design/image-transfer.jpg"
-            },
-            {
-                "name": "photon",
-                "desc": "A state-of-the-art music player with high-resolution audio and DSP effects",
-                "image": "/web-design/image-photon.jpg"
-            },
-            {
-                "name": "builder",
-                "desc": "Connects users with local contractors based on their location",
-                "image": "/web-design/image-builder.jpg"
-            },
-            {
-                "name": "blogr",
-                "desc": "Blogr is a platform for creating an online blog or publication",
-                "image": "/web-design/image-blogr.jpg"
-            },
-            {
-                "name": "camp",
-                "desc": "Get expert training in coding, data, design, and digital marketing",
-                "image": "/web-design/image-camp.jpg"
-            }
-        ]
+        data: ["Web Design", "We build websites that serve as powerful marketing tools and bring memorable brand experiences."]
     },
     {
         path: 'app-design',
         component: DesignPageComponent,
-        data: [
-            {
-                "name": "App Design",
-                "desc": "Our mobile designs bring intuitive digital solutions to your customers right at their fingertips."
-            },
-            {
-                "name": "airfilter",
-                "desc": "Solving the problem of poor indoor air quality by filtering the air",
-                "image": "/app-design/desktop/image-airfilter.jpg"
-            },
-            {
-                "name": "eyecam",
-                "desc": "Product that lets you edit your favorite photos and videos at any time",
-                "image": "/app-design/desktop/image-eyecam.jpg"
-            },
-            {
-                "name": "faceit",
-                "desc": "Get to meet your favorite internet superstar with the faceit app",
-                "image": "/app-design/desktop/image-faceit.jpg"
-            },
-            {
-                "name": "todo",
-                "desc": "A todo app that features cloud sync with light and dark mode",
-                "image": "/app-design/desktop/image-todo.jpg"
-            },
-            {
-                "name": "loopstudios",
-                "desc": "A VR experience app made for Loopstudios",
-                "image": "/app-design/desktop/image-loopstudios.jpg"
-            }
-        ]
+        data: ["App Design", "Our mobile designs bring intuitive digital solutions to your customers right at their fingertips."]
     },
     {
         path: 'graphic-design',
         component: DesignPageComponent,
-        data: [
-            {
-                "name": "Graphic Design",
-                "desc": "We deliver eye-catching branding materials that are tailored to meet your business objectives."
-            },
-            {
-                "name": "tim brown",
-                "desc": "A book cover designed for Tim Brown’s new release, ‘Change’",
-                "image": "/graphic-design/desktop/image-change.jpg"
-            },
-            {
-                "name": "boxed water",
-                "desc": "A simple packaging concept made for Boxed Water",
-                "image": "/graphic-design/desktop/image-boxed-water.jpg"
-            },
-            {
-                "name": "science!",
-                "desc": "A poster made in collaboration with the Federal Art Project",
-                "image": "/graphic-design/desktop/image-science.jpg"
-            }
-        ]
+        data: ["Graphic Design", "We deliver eye-catching branding materials that are tailored to meet your business objectives."]
     }
 
 ];
--- src/app/design-page/design-page.component.ts
+++ src/app/design-page/design-page.component.ts
@@ -1,18 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Data } from '@angular/router';
+import webDesignData from "../data/web-design-data.json"
+import appDesignData from "../data/app-design-data.json"
+import graphicDesignData from "../data/graphic-design-data.json"
+import { DesignCardComponent } from "../design-card/design-card.component";
 
 @Component({
   selector: 'app-design-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, DesignCardComponent],
   templateUrl: './design-page.component.html',
   styleUrl: './design-page.component.css'
 })
-export class DesignPageComponent {
+export class DesignPageComponent implements OnInit {
 
-  constructor(private activatedRoute: ActivatedRoute) {
-    const data: Data = this.activatedRoute.snapshot.data
+  data : any[] | undefined
+  titleData: any
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+      this.route.data.subscribe((data) => {
+        this.titleData = data
+        if (data[0] === "Web Design") {
+          this.data = webDesignData
+        }else if (data[0] === "App Design") {
+          this.data = appDesignData
+        }else if (data[0] === "Graphic Design") {
+          this.data = graphicDesignData
+        }
+      })
+
+      console.log(this.data)
   }
 
 }
