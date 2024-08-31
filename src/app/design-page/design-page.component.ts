189fd2eabd8ade04c3d25da4ebf377375191a627
DeclanLacey <declan.lacey@hotmail.com>
2024-08-31 16:00:32 -0500

    responsive design

--- src/app/design-page/design-page.component.ts
+++ src/app/design-page/design-page.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Data } from '@angular/router';
+import { ActivatedRoute, Data, RouterLink, RouterLinkActive } from '@angular/router';
 import webDesignData from "../data/web-design-data.json"
 import appDesignData from "../data/app-design-data.json"
 import graphicDesignData from "../data/graphic-design-data.json"
@@ -9,7 +9,7 @@ import { DesignCardComponent } from "../design-card/design-card.component";
 @Component({
   selector: 'app-design-page',
   standalone: true,
-  imports: [CommonModule, DesignCardComponent],
+  imports: [CommonModule, DesignCardComponent, RouterLinkActive, RouterLink],
   templateUrl: './design-page.component.html',
   styleUrl: './design-page.component.css'
 })
