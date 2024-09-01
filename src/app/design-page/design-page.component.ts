a7ff20a54cc2dc22cbd979166b7b7f0c222dcd7a
DeclanLacey <declan.lacey@hotmail.com>
2024-09-01 18:28:56 -0500

    Added alt tags to images

--- src/app/design-page/design-page.component.ts
+++ src/app/design-page/design-page.component.ts
@@ -4,12 +4,11 @@ import { ActivatedRoute, Data, RouterLink, RouterLinkActive } from '@angular/rou
 import webDesignData from "../data/web-design-data.json"
 import appDesignData from "../data/app-design-data.json"
 import graphicDesignData from "../data/graphic-design-data.json"
-import { DesignCardComponent } from "../design-card/design-card.component";
 
 @Component({
   selector: 'app-design-page',
   standalone: true,
-  imports: [CommonModule, DesignCardComponent, RouterLinkActive, RouterLink],
+  imports: [CommonModule, RouterLinkActive, RouterLink],
   templateUrl: './design-page.component.html',
   styleUrl: './design-page.component.css'
 })
